ab3fb76c6246e0c0fb02ff061d1621a6a4abdbd3
Michael-Lange <43082768+Michael-lange@users.noreply.github.com>
2024-05-06 15:23:50 -0500

    Added portfolio.js. Updated jQuery version. Added jQuery animation to Experience section.

---
+++ src/scripts/portfolio.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('#education').hide();
+    $(".expAnim").hide();
+    
+    $('#education').toggle(1000);
+    $(".expAnim").slideDown(1200,'swing');
+   
+    
+});
+
